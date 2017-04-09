d01d1d835ea3e3f97580a165ee4f5a47305ebc95
feixia5320 <1044489176@qq.com>
2017-04-09 10:58:21 +0800

    字符串是回文
    收到

---
+++ 03learnjs2.js
@@ -0,0 +1,72 @@
+//--------------------字符串逆序----------------
+function reverseString(str) {
+
+  var arr = str.split('');
+  arr.reverse();
+  var tmp = arr.join('');
+  return tmp;
+}
+
+reverseString("hello");
+
+//-----------------一个整数的阶乘----------------
+function factorialize(num) {
+  var total =1;
+  while(num > 1){
+    total = total * num;
+    num--;
+  }
+  return total;
+}
+
+factorialize(5);
+
+//------------------字符串是回文----------------
+function palindrome(str) {
+  var tmp=[];
+  var t_str;
+  t_str = str.replace(/\,|\s|\.|\-|\_/gi,'');
+  t_str = t_str.replace(/\(/g,')');
+  t_str = t_str.toLowerCase();
+  var t_str2 = reverseString(t_str);
+  if(t_str2==t_str){
+    return true;
+  }else{
+    return false;
+  }
+  
+}
+
+function reverseString(str) {
+  var arr = str.split('');
+  arr.reverse();
+  var tmp = arr.join('');
+  return tmp;
+}
+
+palindrome("0_0 (: /-\ :) 0-0");
+
+//----------------句子中最长的单词----------------
+function findLongestWord(str) {
+  var arr = [];
+  arr = str.split(' ');
+  var arr_len = [];
+  var max = 0;
+  arr.forEach(getlength);
+  arr_len.forEach(getMax);
+
+function getlength(val,index,arr) {
+    arr_len[index] = val.length;
+    return arr_len;
+}
+function getMax(val,index,arr) {
+    if(val > max){
+      max = val;
+    }
+    return max;
+}
+
+  return max;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
