aa9235c3ce5452864bd0a9ac83bbea06a3e8e789
feixia5320 <1044489176@qq.com>
2019-04-16 22:24:38 +0800

    require依赖

--- 11main.js
+++ 11main.js
@@ -1,5 +1,16 @@
-require(['11math'], function (math) {
+require.config({
+    //data-main的路径就是默认的baseUrl
+    baseUrl: "",
+    paths: {
+        "fun1" : "11fun1",
+    }
+});
 
-    alert(math.add(1, 1));
 
+require(["fun1"], function () {
+
+    //fun1必须在require的依赖中
+    let fun = require("fun1");
+    let res = fun.add(1,2);
+    console.log("fun1", res)
 });
